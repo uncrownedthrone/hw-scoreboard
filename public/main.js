f19699e336f11795a211a52b5cd76b8104f44e89
Justin Swiger <justin.swiger@me.com>
2019-11-05 15:55:43 -0500

    finished hw

--- public/main.js
+++ public/main.js
@@ -13,9 +13,10 @@ teamTwoScoreText.textContent = teamTwoScore
 const teamOneAddOne = () => {
   teamOneScore += 1
   teamOneScoreText.textContent = teamOneScore
-  if (teamOneScore > 21) {
-    qs('.team-1-score').textContent = 'You already won!'
-    console.log('player 1 above 21')
+  if (teamOneScore === 21) {
+    qs('.team-1-score').textContent = 'Winner!'
+    qs('button.team-1-add-1-button').disabled = true
+    qs('button.team-1-subtract-1-button').disabled = true
   }
   if (teamOneScore === 21) {
     qs('.team-1-score').textContent = 'You win!'
@@ -26,11 +27,12 @@ const teamOneAddOne = () => {
 const teamTwoAddOne = () => {
   teamTwoScore += 1
   teamTwoScoreText.textContent = teamTwoScore
-  if (teamTwoScore > 21) {
-    qs('.team-2-score').textContent = 'You already won!'
-    console.log('player 2 above 21')
-  }
   if (teamTwoScore === 21) {
+    qs('.team-2-score').textContent = 'Winner!'
+    qs('button.team-2-add-1-button').disabled = true
+    qs('button.team-2-subtract-1-button').disabled = true
+  }
+  if (teamOneScore === 21) {
     qs('.team-2-score').textContent = 'You win!'
     console.log('player 2 wins')
   }
@@ -73,51 +75,30 @@ const updateTeamTwoName = () => {
 
 // // add reset button to reset to 0 and enable buttons
 const resetGame = () => {
-  if (teamOneScore === 21) {
-    qs('.reset-button').addEventListener('click', resetGame)
-  }
-  console.log('i clicked reset')
+  const theResetMode = 0
+  teamOneScore = 0
+  teamTwoScore = 0
+  qs('button.team-1-add-1-button').disabled = false
+  qs('button.team-2-add-1-button').disabled = false
+  qs('button.team-1-subtract-1-button').disabled = false
+  qs('button.team-2-subtract-1-button').disabled = false
+  document.querySelector('.team-2-score').textContent = theResetMode
+  document.querySelector('.team-1-score').textContent = theResetMode
 }
 
-const main = () => {
-  qs('button.team-1-add-1-button').addEventListener('click', teamOneAddOne)
-  qs('button.team-1-subtract-1-button').addEventListener(
-    'click',
-    teamOneSubtractOne
-  )
-  qs('button.team-2-add-1-button').addEventListener('click', teamTwoAddOne)
-  qs('button.team-2-subtract-1-button').addEventListener(
-    'click',
-    teamTwoSubtractOne
-  )
-  qs('button.update-team-1-name').addEventListener('click', updateTeamOneName)
-  qs('button.update-team-2-name').addEventListener('click', updateTeamTwoName)
-  resetGame()
-}
-
-const startTimer = (duration, display) => {
-  let timer = duration,
-    minutes,
-    seconds
-  setInterval(function() {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10)
-
-    minutes = minutes < 10 ? '0' + minutes : minutes
-    seconds = seconds < 10 ? '0' + seconds : seconds
-
-    display.textContent = minutes + ':' + seconds
-
-    if (--timer < 0) {
-      timer = duration
-    }
-  }, 1000)
-}
-
-window.onload = function() {
-  const fiveMinutes = 60 * 5,
-    display = qs('.time')
-  startTimer(fiveMinutes, display)
-}
+qs('button.team-1-add-1-button').addEventListener('click', teamOneAddOne)
+qs('button.team-1-subtract-1-button').addEventListener(
+  'click',
+  teamOneSubtractOne
+)
+qs('button.team-2-add-1-button').addEventListener('click', teamTwoAddOne)
+qs('button.team-2-subtract-1-button').addEventListener(
+  'click',
+  teamTwoSubtractOne
+)
+qs('button.update-team-1-name').addEventListener('click', updateTeamOneName)
+qs('button.update-team-2-name').addEventListener('click', updateTeamTwoName)
+resetGame()
+qs('button.reset-button').addEventListener('click', resetGame)
 
 document.addEventListener('DOMContentLoaded', main)
